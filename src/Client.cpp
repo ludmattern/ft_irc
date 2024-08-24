7b22a0a2d2deacc93757049ac4da29fbeeab6d53
ludmattern <github.rfgrg@passmail.net>
2024-08-24 13:19:21 +0200

    refactoring cpp files

--- src/Client.cpp
+++ src/Client.cpp
@@ -6,25 +6,31 @@
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 18:20:56 by lmattern          #+#    #+#             */
-/*   Updated: 2024/08/20 18:21:12 by lmattern         ###   ########.fr       */
+/*   Updated: 2024/08/21 17:42:47 by lmattern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
 
-Client::Client() {
+Client::Client(int fd, const std::string &ip_address)
+    : _fd(fd), _ip_address(ip_address), _authenticated(false) {}
+
+int Client::getFd() const
+{
+    return (_fd);
 }
 
-Client::Client(const Client& other) {
-    memberVar = other.memberVar;
+const std::string &Client::getIpAddress() const
+{
+    return (_ip_address);
 }
 
-Client& Client::operator=(const Client& other) {
-    if (this != &other) {
-        memberVar = other.memberVar;
-    }
-    return *this;
+bool Client::isAuthenticated() const
+{
+    return (_authenticated);
 }
 
-Client::~Client() {
+void Client::authenticate()
+{
+    _authenticated = true;
 }
--- src/ServerUtils.cpp
+++ src/ServerUtils.cpp
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Channel.cpp                                        :+:      :+:    :+:   */
+/*   ServerUtils.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/20 18:20:56 by lmattern          #+#    #+#             */
-/*   Updated: 2024/08/20 18:21:31 by lmattern         ###   ########.fr       */
+/*   Created: 2024/08/21 17:31:24 by lmattern          #+#    #+#             */
+/*   Updated: 2024/08/21 17:43:20 by lmattern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Server.hpp"
+#include <sstream>
 
-#include "Channel.hpp"
-
-Channel::Channel() {
-}
-
-Channel::Channel(const Channel& other) {
-    memberVar = other.memberVar;
-}
-
-Channel& Channel::operator=(const Channel& other) {
-    if (this != &other) {
-        memberVar = other.memberVar;
-    }
-    return *this;
-}
-
-Channel::~Channel() {
+std::string ft_to_string(int value)
+{
+	std::ostringstream oss;
+	oss << value;
+	return (oss.str());
 }
