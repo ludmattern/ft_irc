e7b0171c19c8dba260b05d667dffb6b20692c6aa
Felix Prevot <fprevot@z2r4p6.42lyon.fr>
2024-09-28 14:55:32 +0200

    add cmd Utils

--- src/Command/Nick.cpp
+++ src/Command/Nick.cpp
@@ -3,33 +3,6 @@
 #include "Client.hpp"
 #include "Channel.hpp"
 
-bool Server::isNicknameTaken(const std::string& nickname) const
-{
-	for (std::map<int, Client*>::const_iterator it = _clients.begin(); it != _clients.end(); ++it)
-	{
-		if (it->second->getNickname() == nickname)
-			return true;
-	}
-	return false;
-}
-
-void Server::registerClientIfReady(Client* client)
-{
-	if (client->getNickname().empty() || client->getUsername().empty())
-		return;
-
-	if (!_password.empty())
-	{
-		if (client->getPassword() != _password)
-		{
-			sendError(client, ERR_PASSWDMISMATCH, "", "Password incorrect");
-			return ;
-		}
-	}
-
-	client->setRegistered(true);
-	sendWelcomeMessages(client);
-}
 
 void Nick::execute(Server& server, Client& client, const std::vector<std::string>& params)
 {
