0c983a50ecd9248f61eba431a31a3f6a37d68345
ludmattern <github.rfgrg@passmail.net>
2024-09-30 09:30:23 +0200

    patch the BRED error

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:32:29 by lmattern          #+#    #+#             */
-/*   Updated: 2024/09/25 09:52:59 by lmattern         ###   ########.fr       */
+/*   Updated: 2024/09/30 09:29:47 by lmattern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << BRED"Error: "RESET << RED << e.what() << RESET << std::endl;
+		std::cerr << BRED "Error: " RESET << RED << e.what() << RESET << std::endl;
 		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
