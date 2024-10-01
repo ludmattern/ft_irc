2fdb2f05112835fee505fb4e993a3ddca3315840
ludmattern <github.rfgrg@passmail.net>
2024-10-01 11:53:22 +0200

    headers

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -6,7 +6,7 @@
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 08:20:05 by lmattern          #+#    #+#             */
-/*   Updated: 2024/09/30 08:20:07 by lmattern         ###   ########.fr       */
+/*   Updated: 2024/10/01 11:24:02 by lmattern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ class Server;
 
 class Parser
 {
-    private:
-
-        std::map<std::string, Command*> _commands;
     public:
 
         Parser();
         ~Parser();
         void executeCommand(const std::string& commandName, Server& server, Client& client, const std::vector<std::string>& params);
+    private:
+
+        std::map<std::string, Command*> _commands;
 
 };
 
