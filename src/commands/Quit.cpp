1168a3d16352e61fe80e7c2e309ac317780c5880
Felix Prevot <fprevot@z2r2p6.42lyon.fr>
2024-10-02 15:26:14 +0200

    add quit

--- src/commands/Quit.cpp
+++ src/commands/Quit.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Quit.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fprevot <fprevot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 11:29:46 by lmattern          #+#    #+#             */
-/*   Updated: 2024/10/02 11:29:47 by lmattern         ###   ########.fr       */
+/*   Updated: 2024/10/02 15:25:52 by fprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,26 @@
 #include <vector>
 #include <algorithm>
 
-Quit::Quit() {}
+Quit::Quit() : Command() {}
 
 Quit::~Quit() {}
 
 void Quit::execute(Client& client, const std::vector<std::string>& params) 
 {
-    (void)client;
-    (void)params;
-    std::cout << "quit triggered\n";
+	std::string quitMessage = "Client exited";
+	if (!params.empty()) 
+    {
+		quitMessage = params[0];
+	}
+
+	std::string prefix = client.getPrefix();
+	std::string message = ":" + prefix + " QUIT";
+	if (!quitMessage.empty()) 
+    { 
+		message += " :" + quitMessage;
+	}
+    //add chan leaving and broadcast msg
+    
+    _server.closeClientConnection(client.getFd());
+    
 }
