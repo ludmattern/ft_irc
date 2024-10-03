2e429108f082b461191b08de752e3f68d5060637
Felix Prevot <fprevot@z4r2p6.42lyon.fr>
2024-10-03 17:27:14 +0200

    mode parsing

--- src/commands/Mode.cpp
+++ src/commands/Mode.cpp
@@ -6,7 +6,7 @@
 /*   By: fprevot <fprevot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 11:29:38 by lmattern          #+#    #+#             */
-/*   Updated: 2024/10/03 16:50:00 by fprevot          ###   ########.fr       */
+/*   Updated: 2024/10/03 17:25:57 by fprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,47 @@
 #include "network/Channel.hpp"
 #include "commands/Command.hpp"
 #include "replies.hpp"
-
+#include <iostream>
 Mode::Mode() {}
 Mode::~Mode() {}
 
 void Mode::execute(Client* client, const std::vector<std::string>& params)
 {
-    if (params.size() < 2) 
+	if (params.size() < 2) 
 	{
-        client->reply(ERR_NEEDMOREPARAMS(client->getNickname(), "MODE"));
-        return;
-    }
+		client->reply(ERR_NEEDMOREPARAMS(client->getNickname(), "MODE"));
+		return;
+	}
 
 	std::string channelName = params[0];
-    std::string modeString = params[1];
-	
+	std::string modeString = params[1];
+
+	Channel* channel = _server.getChannelByName(channelName);
+	if (!channel) 
+	{
+		client->reply(ERR_NOSUCHCHANNEL(client->getNickname(), channelName));
+		return;
+	}
+	if (!channel->isOperator(client)) 
+	{
+		client->reply(ERR_CHANOPRIVSNEEDED(client->getNickname(), channelName));
+		return;
+	}
+	char operation = modeString[0];
+	if (operation != '-' && operation != '+')
+	{
+		client->reply(ERR_UMODEUNKNOWNFLAG(client->getNickname()));
+		return;
+	}
+	char mode = modeString[1];
+	if (mode != 'i' && mode != 't' && mode != 'k' && mode != 'o' && mode != 'l')
+	{
+		client->reply(ERR_UMODEUNKNOWNFLAG(client->getNickname()));
+		return;
+	}
+	if (modeString.size() > 2) 
+	{
+		client->reply(ERR_UMODEUNKNOWNFLAG(client->getNickname()));
+		return;
+	}
 }
